82b871a10b896ce4543940206d009a9e775f36cb
Shyrone <75037137+Shyrone@users.noreply.github.com>
2020-12-04 22:59:19 +0700

    Update PesanMakanan.java

--- PesanMakanan.java
+++ PesanMakanan.java
@@ -6,6 +6,7 @@
 import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -27,131 +28,176 @@ public class PesanMakanan {
     private static Connection connection;
     private static PreparedStatement tambahData;
 
-	public static void main(String[] args) {
-	
-	Scanner input = new Scanner(System.in);
-	ArrayList<String> pesanan = new ArrayList<String>();
-	ArrayList<Integer> harga_pesanan = new ArrayList<Integer>();
-	Random r = new Random();
-	
-	//List Menu
-	String [] menu = {" ", "Nasi Goreng", "Mie Goreng", "Mie Ayam", "Seblak", "Bakso", "Es Teh Manis", "Lemon Tea", "Air Mineral"};
-	int [] h_menu = {0, 18000, 15000, 17000, 15000, 17000, 5000, 7000, 3000};
+    public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    Scanner pilihan = new Scanner(System.in);
+    ArrayList<String> pesanan = new ArrayList<String>();
+    ArrayList<Integer> harga_pesanan = new ArrayList<Integer>();
+    Random r = new Random();
+    
+    //List Menu
+    String [] menu = {" ", "Nasi Goreng", "Mie Goreng", "Mie Ayam", "Seblak", "Bakso", "Es Teh Manis", "Lemon Tea", "Air Mineral"};
+    int [] h_menu = {0, 18000, 15000, 17000, 15000, 17000, 5000, 7000, 3000};
 
-	try{
-		connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-	}catch (SQLException e){ 
-		throw new RuntimeException(e);
- 	}
+    try{
+        connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+    }catch (SQLException e){ 
+        throw new RuntimeException(e);
+    }
 
- 	//Data diri pengguna yang diperlukan
- 	String nama;
- 	String noHP;
- 	String alamat;
+    //Data diri pengguna yang diperlukan
+    String nama;
+    String noHP;
+    String alamat;
 
- 	//Tampilan Menu
- 	System.out.println("Selamat Datang Di Apilikasi Pesan Makanan Kelompok 6");
- 	System.out.println("Berikut adalah menu yang tersedia");
- 	System.out.println("----------------- MENU ----------------");
- 	System.out.println("No |Item                       |Harga");
- 	System.out.println("1. Nasi Goreng_________________Rp.18000");
- 	System.out.println("2. Mie Goreng__________________Rp.15000");
- 	System.out.println("3. Mie Ayam____________________Rp.17000");
- 	System.out.println("4. Seblak______________________RP.15000");
- 	System.out.println("5. Bakso_______________________Rp.17000");
- 	System.out.println("6. Es Teh Manis________________Rp.5000");
- 	System.out.println("7. Lemon Tea___________________Rp.7000");
- 	System.out.println("8. Air Mineral_________________Rp.3000\n\n\n");
+    //Tampilan Menu
+    System.out.println("Selamat Datang Di Apilikasi Pesan Makanan Kelompok 6");
+    System.out.println("Berikut adalah menu yang tersedia");
+    System.out.println("----------------- MENU ----------------");
+    System.out.println("No |Item                       |Harga");
+    System.out.println("1. Nasi Goreng_________________Rp.18000");
+    System.out.println("2. Mie Goreng__________________Rp.15000");
+    System.out.println("3. Mie Ayam____________________Rp.17000");
+    System.out.println("4. Seblak______________________RP.15000");
+    System.out.println("5. Bakso_______________________Rp.17000");
+    System.out.println("6. Es Teh Manis________________Rp.5000");
+    System.out.println("7. Lemon Tea___________________Rp.7000");
+    System.out.println("8. Air Mineral_________________Rp.3000\n\n");
+        
+        System.out.println("Apa yang ingin anda lakukan?");
+        System.out.println("1. Lakukan Pemesanan");
+        System.out.println("2. Lihat Riwayat Pemesanan");
+        System.out.print("\nPilih :");
+        int pilih = pilihan.nextInt();
+        
+        
+        if(pilih == 1){
+            
+            String yon; //pilihan yes or no
+            int item;   //pilihan menu
+            int n;      //jumlah yang ingin dipesan
+            int totalharga = 0;
+            boolean status = true;
+            String kodeTransaksi;
+            int nilaiacak = 1 + r.nextInt(10000);
 
+            do{
+                    System.out.print("Lakukan Pemesnaan Makanan (Y/N) ? ");
+                    yon = input.nextLine();
+                    if(yon.equalsIgnoreCase("Y") || yon.equalsIgnoreCase("Yes")){
 
- 	String yon;	//pilihan yes or no
- 	int item;	//pilihan menu
- 	int n;		//jumlah yang ingin dipesan
- 	int totalharga = 0;
- 	boolean status = true;
- 	String kodeTransaksi;
- 	int nilaiacak = 1 + r.nextInt(10000);
+                            System.out.print("Masukkan Nama Pemesan     :"); nama   = input.nextLine();
+                            System.out.print("Masukkan Nomor HP     :"); noHP   = input.nextLine();
+                            System.out.print("Masukkan alamat   :"); alamat = input.nextLine();
 
- 	do{
- 		System.out.print("Lakukan Pemesnaan Makanan (Y/N) ? ");
- 		yon = input.nextLine();
- 		if(yon.equalsIgnoreCase("Y") || yon.equalsIgnoreCase("Yes")){
+                            boolean status2 = true;
+                            String yon2;
+                            String yon3;
+                            do{
+                                    System.out.print("\n\nApa yang ingin anda Pesan ? (Masukkan Nomor item Pada Menu) ");
+                                    item = input.nextInt();
+                                    if(item > 0 && item < menu.length){
+                                        System.out.print("Berapa jumlah " +  menu[item] + " yang anda inginkan? ");
+                                        n = input.nextInt();
 
- 			System.out.print("Masukkan Nama Pemesan 	:"); nama   = input.nextLine();
- 			System.out.print("Masukkan Nomor HP 	:"); noHP   = input.nextLine();
- 			System.out.print("Masukkan alamat 	:"); alamat = input.nextLine();
- 			
- 			boolean status2 = true;
- 			String yon2;
- 			String yon3;
- 			do{
- 				System.out.print("\n\nApa yang ingin anda Pesan ? (Masukkan Nomor item Pada Menu) ");
- 				item = input.nextInt();
- 				System.out.print("Berapa jumlah " +  menu[item] + " yang anda inginkan? ");
- 				n = input.nextInt();
+                                        for (int i=1; i <= n; i++){
+                                                pesanan.add(menu[item]);
+                                                harga_pesanan.add(h_menu[item]);
+                                        }
 
- 				for (int i=1; i <= n; i++){
- 					pesanan.add(menu[item]);
- 					harga_pesanan.add(h_menu[item]);
- 				}
+                                        //cek pesanan
+                                        //System.out.println(pesanan.toString());
+                                        //System.out.println(harga_pesanan.toString());
+                                        System.out.print("\n\nApakah masih ada tambahan (Y/N) ? ");
+                                        yon2 = input.next();
 
- 				//cek pesanan
- 				//System.out.println(pesanan.toString());
- 				//System.out.println(harga_pesanan.toString());
- 				
- 				System.out.print("\n\nApakah masih ada tambahan (Y/N) ? ");
- 				yon2 = input.next();
+                                        if(yon2.equalsIgnoreCase("N") || yon2.equalsIgnoreCase("No")){
+                                                status2 = false;
+                                        }
 
- 				if(yon2.equalsIgnoreCase("N") || yon2.equalsIgnoreCase("No")){
- 					status2 = false;
- 				}
- 			}while(status2);
+                                    }else{
+                                        System.out.println("Maaf pilihan tidak tersedia");
+                                    }
 
- 			System.out.println("Berikut Pesanan Yang anda buat\n");
- 			System.out.println("Item           |Harga");
- 			for(int i = 0; i < pesanan.size(); i++){
- 				System.out.println(pesanan.get(i)+"\t"+harga_pesanan.get(i));
- 				totalharga = totalharga + harga_pesanan.get(i);
- 			}
- 			System.out.println("Total Harga:\t" + totalharga);
+                            }while(status2);
 
- 			System.out.print("\n\nKorfirmasi Pemesanan anda (Y/N) ? ");
- 			yon3 = input.next();
- 			kodeTransaksi = (nama.substring(0,2)).toUpperCase() + noHP.substring(noHP.length()-2) + (alamat.substring(alamat.length()-2)).toUpperCase() + nilaiacak;
-                        try {
-                            tambahData = connection.prepareStatement("INSERT INTO transaksi (Kode_Transaksi, Nama_Pemesan, No_Telp, Alamat, Daftar_Pesanan, Total_Harga)" + " VALUES (?, ?, ?, ?, ?, ?)");
-                            tambahData.setString(1, kodeTransaksi);
-                            tambahData.setString(2, nama);
-                            tambahData.setString(3, noHP);
-                            tambahData.setString(4, alamat);
-                            tambahData.setString(5, pesanan.toString());
-                            tambahData.setInt(6, totalharga);
-                            int result = tambahData.executeUpdate();
-                        } catch (SQLException ex) {
-                            ex.printStackTrace();
+                            System.out.println("Berikut Pesanan Yang anda buat\n");
+                            System.out.println("Item           |Harga");
+                            for(int i = 0; i < pesanan.size(); i++){
+                                    System.out.println(pesanan.get(i)+"\t"+harga_pesanan.get(i));
+                                    totalharga = totalharga + harga_pesanan.get(i);
+                            }
+                            System.out.println("Total Harga:\t" + totalharga);
+
+                            System.out.print("\n\nKorfirmasi Pemesanan anda (Y/N) ? ");
+                            yon3 = input.next();
+                            kodeTransaksi = (nama.substring(0,2)).toUpperCase() + noHP.substring(noHP.length()-2) + (alamat.substring(alamat.length()-2)).toUpperCase() + nilaiacak;
+                            try {
+                                tambahData = connection.prepareStatement("INSERT INTO transaksi (Kode_Transaksi, Nama_Pemesan, No_Telp, Alamat, Daftar_Pesanan, Total_Harga)" + " VALUES (?, ?, ?, ?, ?, ?)");
+                                tambahData.setString(1, kodeTransaksi);
+                                tambahData.setString(2, nama);
+                                tambahData.setString(3, noHP);
+                                tambahData.setString(4, alamat);
+                                tambahData.setString(5, pesanan.toString());
+                                tambahData.setInt(6, totalharga);
+                                int result = tambahData.executeUpdate();
+                            } catch (SQLException ex) {
+                                ex.printStackTrace();
+                            }
+
+                            //cek kode
+                            //System.out.println(kodeTransaksi);
+
+                            if(yon3.equalsIgnoreCase("Y") || yon3.equalsIgnoreCase("Yes")){
+                                    System.out.println("\nPilih metode pembayaran anda");
+                                    System.out.println("1. Debit");
+                                    System.out.println("2. Tunai");
+
+                            }else{
+                                    System.out.println("\nPesanan Anda Akan dibatalakan");
+                                    System.out.println("Terima Kasih Telah Menggunakan");
+                                    System.exit(0);
+                            }
+
+                    }else if(yon.equalsIgnoreCase("N") || yon.equalsIgnoreCase("No")){
+
+                            System.out.println("Terima Kasih telah Menggunakan");
+                            System.exit(0);
+
+                    }else{
+
+                            System.out.println("Masukkan tidak sesuai");
+
+                    }
+            }while(!yon.equalsIgnoreCase("Y") && !yon.equalsIgnoreCase("N") && !yon.equalsIgnoreCase("Yes") && !yon.equalsIgnoreCase("No"));
+      
+        }else{
+            System.out.println("\nMasukkan Kode Pemesanan");
+            String kode = input.next();
+                try{
+                    PreparedStatement stmt = connection.prepareStatement("select * from transaksi where Kode_Transaksi= '"+kode+"'");  
+                    ResultSet rs = stmt.executeQuery();
+                    DatabaseMetaData md = connection.getMetaData();
+                    ResultSet rs2 = md.getColumns(null, null, "transaksi", "Kode_Transaksi");
+                    
+                    if (rs.next()) {
+                        while(rs2.next()){
+                            System.out.println("\nKode Transaksi  : "+rs.getString(1));
+                            System.out.println("Nama Pemesan    : "+rs.getString(2));
+                            System.out.println("No. Telp        : "+rs.getString(3));
+                            System.out.println("Alamat          : "+rs.getString(4));
+                            System.out.println("Daftar Pesanan  : "+rs.getString(5));
+                            System.out.println("Total Harga     : "+rs.getInt(6));
                         }
- 			
- 			//cek kode
- 			//System.out.println(kodeTransaksi);
-                        
- 			if(yon3.equalsIgnoreCase("Y") || yon3.equalsIgnoreCase("Yes")){
- 				System.out.println("\nPilih metode pembayaran anda");
- 				System.out.println("1. Debit");
- 				System.out.println("2. Tunai");
-
- 			}else{
- 				System.out.println("\nPesanan Anda Akan dibatalakan");
- 				System.out.println("Terima Kasih Telah Menggunakan");
- 				System.exit(0);
- 			}
-
- 		}else{
- 			System.out.println("Terima Kasih telah Menggunakan");
- 			System.exit(0);
- 		}
- 	}while(!yon.equalsIgnoreCase("Y") && !yon.equalsIgnoreCase("N") && !yon.equalsIgnoreCase("Yes") && !yon.equalsIgnoreCase("No"));
-
-
+                    }else{
+                        System.out.println("Maaf riwayat pembelian tidak ditemukan.");
+                    }
+                }catch(Exception e){
+                    
+                }
+            
         }
     
+        }
 }
